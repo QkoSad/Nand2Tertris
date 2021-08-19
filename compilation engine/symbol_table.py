ebca7f36e3b92d5fab924e27e2fa7026818fd440
QkoSad <88448470+QkoSad@users.noreply.github.com>
2021-08-19 17:35:36 +0300

    Update symbol_table.py
    now there is a difference between neg and sub

--- compilation engine/symbol_table.py
+++ compilation engine/symbol_table.py
@@ -1,55 +1,75 @@
-class Symbol:
-    def __init__(self):
-        self.s_name = ''
-        self.s_kind = ''
-        self.s_type = ''
-        self.s_index = 0
+class VMWriter:
+    def __init__(self, vm_path):
+        self.my_vm = open(vm_path, "w+")
+        self.label_index = 0
 
+    def write_push(self, segment, index):
+        self.my_vm.write(f'push {segment} {index}\n')
+        print(f'push {segment} {index}')
 
-class SymbolTable:
-    def __init__(self):
-        self.sym = []
+    def write_lable(self, label):
+        self.my_vm.write(f'label L{label}\n')
+        print(f'label L{label}')
 
-    def define(self, s_name, s_type, s_kind):
-        self.sym.append(Symbol())
-        self.sym[-1].s_name = s_name
-        self.sym[-1].s_type = s_type
-        self.sym[-1].s_kind = s_kind
-        self.sym[-1].s_index = self.var_count(s_kind) - 1
+    def write_arithmetic(self, command):
+        if command == '+':
+            self.my_vm.write('add\n')
+            print('add')
+        elif command == '-':
+            self.my_vm.write('sub\n')
+            print('sub')
+        elif command == 'neg':
+            self.my_vm.write('neg\n')
+            print('neg')
+        elif command == '=':
+            self.my_vm.write('eq\n')
+            print('eq')
+        elif command == '>':
+            self.my_vm.write('gt\n')
+            print('gt')
+        elif command == '<':
+            self.my_vm.write('lt\n')
+            print('lt')
+        elif command == '&':
+            self.my_vm.write('and\n')
+            print('and')
+        elif command == '|':
+            self.my_vm.write('or\n')
+            print('or')
+        elif command == '~':
+            self.my_vm.write('not\n')
+            print('not')
+        elif command == '*':
+            self.write_call('Math.multiply', 2)
+        elif command == '/':
+            self.write_call('Math.divide', 2)
 
-    def start_subroutine(self, s_name, s_type):
-        self.sym.append(Symbol())
-        self.sym[-1].s_name = s_name
-        self.sym[-1].s_type = s_type
-        self.sym[-1].s_kind = 'argument'
-        self.sym[-1].s_index = 0
+    def write_pop(self, segment, index):
+        print(f'pop {segment} {index}')
+        self.my_vm.write(f'pop {segment} {index}\n')
 
-    def var_count(self, s_kind):
-        count = 0  # it is -1 so the first index is 0
-        for i in self.sym:
-            if i.s_kind == s_kind:
-                count += 1
-        return count
+    def write_goto(self, label):
+        self.my_vm.write(f'goto L{label}\n')
+        print(f'goto L{label}')
 
-    def kind_of(self, s_name):
-        for i in self.sym:
-            if i.s_name == s_name:
-                if i.s_kind == 'var':
-                    return 'local'
-                elif i.s_kind == 'field':
-                    return 'this'
-                else:
-                    return i.s_kind
-        return None
+    def write_if(self, label):
+        self.write_arithmetic('~')
+        self.my_vm.write(f'if-goto L{label}\n')
+        print(f'if-goto L{label}')
 
-    def type_of(self, s_name):
-        for i in self.sym:
-            if i.s_name == s_name:
-                return i.s_type
-        return None
+    def write_call(self, label, num_args):
+        self.my_vm.write(f'call {label} {num_args}\n')
+        print(f'call {label} {num_args}')
 
-    def index_of(self, s_name):
-        for i in self.sym:
-            if i.s_name == s_name:
-                return i.s_index
-        return None
+    def write_function(self, label, num_locals):
+        self.my_vm.write(f'function {label} {num_locals}\n')
+        print(f'function {label} {num_locals}')
+
+    def write_return(self, func_type):
+        if func_type == 'void':
+            self.write_push('constant', '0')
+        self.my_vm.write('return\n')
+        print(f'return')
+
+    def close_vm_file(self):
+        self.my_vm.close()
